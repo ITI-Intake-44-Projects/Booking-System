cf520cd114afbfe1a64aa7d4cd7e53ca8767e4b2
Yousef-Farouk <yfrouk853@gmail.com>
2024-03-26 22:00:29 +0200

    before pull login

---
+++ BookingSystem/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using BookingSystem.Models;
+using BookingSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+namespace BookingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> UserManager,SignInManager<ApplicationUser> SignInManager)
+        {
+            userManager = UserManager;
+            signInManager = SignInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserVM userVM)
+        {
+            if(ModelState.IsValid == true)
+            {
+                //if (userVM.Image != null && userVM.Image.ContentLength > 0)
+                //{
+                //    using (var binaryReader = new BinaryReader(model.ImageFile.InputStream))
+                //    {
+                //        imageData = binaryReader.ReadBytes(model.ImageFile.ContentLength);
+                //    }
+                //}
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = userVM.UserName,
+                    Email = userVM.Email,
+                    PhoneNumber = userVM.PhoneNumber,
+                    FirstName = userVM.FirstName,
+                    LastName = userVM.LastName,
+                    City = userVM.City,
+                    Country = userVM.Country,
+                    //Image = fi,
+                    PasswordHash = userVM.Password,
+                    Address = userVM.Address,
+                };
+                IdentityResult result = await userManager.CreateAsync(user,userVM.Password);
+
+                if (result.Succeeded) 
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var item in result.Errors)
+                    ModelState.AddModelError("", item.Description);
+            }
+            return View("Register",userVM);
+        }
+
+
+    }
+}
---
+++ BookingSystem/ViewModels/RegisterUserVM.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.ViewModels
+{
+    public class RegisterUserVM
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
