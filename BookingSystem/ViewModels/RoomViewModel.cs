cfe9930c96253b03723d5176c087034fe4ff5a73
KhloudHisham <khokhakammar@gmail.com>
2024-04-03 01:37:07 +0200

    "add room view model"

---
+++ BookingSystem/ViewModels/RoomViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookingSystem.ViewModels
+{
+    public class RoomViewModel
+    {
+
+        public int RoomID { get; set; }
+        public string RoomType { get; set; }
+        public decimal PriceOfNight { get; set; }
+        public string HotelName { get; set; }
+        public string? Description { get; set; }
+        public string Image { get; set; }
+    }
+}
